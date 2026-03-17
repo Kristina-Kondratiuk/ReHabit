5a9f4aa17d478e58b601203141fc618f51104708
Kristina-Kondratiuk <kondrk4717913_aehit@students.vizja.pl>
2026-03-17 13:21:00 +0100

    Login Screen

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,12 +1,15 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import React from "react"
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
-export const login = () => {
+export default function Login() {
   return (
     <View style={styles.cont}>
         <Text style={styles.text}>Zaloguj się</Text>
-        <TextInput style={styles.email}/>
-        <TextInput style={styles.pass}/>
-        <Button style={styles.button} title="Zaloguj się"/>
+        <TextInput style={styles.input} placeholder="Email"/>
+        <TextInput style={styles.input} placeholder="Password"/>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Zaloguj się</Text>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -14,12 +17,38 @@ export const login = () => {
 const styles = StyleSheet.create({
     cont: {
         flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
+        padding: 20,
         color: '#f1f1f1'
     },
     text: {
-        fontSize: 24,
+        fontSize: 50,
         fontWeight: 500,
-        color: 'black',
+        color: '#000000',
+        marginBottom: 20
+    },
+    input: {
+      backgroundColor: 'gray',
+      padding: 12,
+      borderWidth: 1,
+      borderRadius: 40,
+      width: '100%',
+      height: 60,
+      marginBottom: 20
+    },
+    button: {
+      backgroundColor: 'blue',
+      paddingVertical: 12,
+      borderRadius: 40,
+      display: 'flex',
+      width: '100%',
+      height: 60,
+      justifyContent: 'center',
+    },
+    buttonText: {
+      color: '#ffffff',
+      fontSize: 24,
+      marginHorizontal: 'auto'
     }
 })
